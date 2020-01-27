1aad7a5fb06c6218c16aba9b69b882fdff4f12b0
Maroof Shittu <dbugshe2@gmail>
2020-01-27 13:57:03 +0100

    fix: use es6 import for unresolved blindman svg 404

--- src/components/GoogleMap/Marker.jsx
+++ src/components/GoogleMap/Marker.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Marker.css';
+import Blind from '../../assets/blind.svg'
 
 const Marker = (props: any) => {
   const { name, id } = props;
   return (
     <div
       className="marker"
-      style={{ backgroundImage: 'url(./images/blind.svg)' , cursor: 'pointer' }}
+      style={{ backgroundImage: Blind , cursor: 'pointer' }}
       title={name}
     />
   );
